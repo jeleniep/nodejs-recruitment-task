f419bb953e26a4c80e56e459d3a2e812c9bde923
jeleniep <piom20@gmail.com>
2021-01-26 08:07:59 +0100

    Last test for movies

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -10,7 +10,7 @@ const {
 
 describe("Movies test", () => {
   let db;
-  
+
   beforeAll(async () => {
     db = await initializeDb(global.__MONGO_URI__)
     let basicUser = new User({ username: "basic_tom", password: "basic", role: "basic", id: 3, name: 'basic' });
@@ -41,7 +41,35 @@ describe("Movies test", () => {
     expect(response.statusCode).toEqual(200)
   });
 
-  
+  test("Test if basic user could add at most 5 movies.", async () => {
+    let response;
+    const titles = ["Tom&Jerry", "Go", "Makbet", "Potop", "It"]
+    for (const title of titles) {
+      let request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/movies',
+        body: {
+          title
+        }
+      });
+      request.userDetails =
+      {
+        userId: 3,
+        name: 'basic',
+        role: 'basic',
+        iat: 1611619287,
+        exp: 1611621087,
+        iss: 'https://www.netguru.com/',
+        sub: '434'
+      }
+
+      response = httpMocks.createResponse();
+      await (addMovie(API_URL, API_KEY))(request, response)
+    }
+    expect(response.statusCode).toEqual(403)
+  });
+
+
   afterAll(async () => {
     await db.close()
   });
